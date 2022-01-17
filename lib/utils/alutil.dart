41109dbb4a8769274cb6760c9ef84fb7a3a25e8d
prince jain <55597203+princ1211@users.noreply.github.com>
2022-01-17 21:28:47 +0530

    Update

--- lib/utils/alutil.dart
+++ lib/utils/alutil.dart
@@ -3,5 +3,6 @@ import 'package:velocity_x/velocity_x.dart';
 mixin Alcolor{
   static Color pricolor1= Vx.orange400;
   static Color pricolor2= Vx.purple300;
+  static Color pricolor3= Vx.orange200;
 
 }
